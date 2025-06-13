6740afbb20d4974ebf592f5ea6d2995577af43ee
JonathanKarni165 <yonatankarni165@gmail.com>
2025-06-13 23:30:26 +0300

    simple functions

---
+++ FibonacciHeap.java
@@ -0,0 +1,162 @@
+import java.util.*;
+/**
+ * FibonacciHeap
+ *
+ * An implementation of Fibonacci heap over positive integers.
+ *
+ */
+public class FibonacciHeap
+{
+	public static void main(String[] args){
+		System.out.println("hi");
+	}
+	public HeapNode min;
+	private int c;
+	private ArrayList<HeapNode> rootList;
+	private HeapNode minNode;
+	private int size;
+	private int linkCount;
+	private int cutCount;
+	
+	/**
+	 *
+	 * Constructor to initialize an empty heap.
+	 * pre: c >= 2.
+	 *
+	 */
+	public FibonacciHeap(int c)
+	{
+		this.c = c;
+		this.rootList = new ArrayList<HeapNode>();
+	}
+
+	/**
+	 * 
+	 * pre: key > 0
+	 *
+	 * Insert (key,info) into the heap and return the newly generated HeapNode.
+	 *
+	 */
+	public HeapNode insert(int key, String info) 
+	{   
+		HeapNode newNode = new HeapNode();
+		newNode.key = key;
+		newNode.info = info; 
+		this.rootList.add(0,newNode);
+		return newNode;
+	}
+
+	/**
+	 * 
+	 * Return the minimal HeapNode, null if empty.
+	 *
+	 */
+	public HeapNode findMin()
+	{
+		return this.minNode; // should be replaced by student code
+	}
+
+	/**
+	 * 
+	 * Delete the minimal item.
+	 * Return the number of links.
+	 *
+	 */
+	public int deleteMin()
+	{
+		return 46; // should be replaced by student code
+
+	}
+
+	/**
+	 * 
+	 * pre: 0<diff<x.key
+	 * 
+	 * Decrease the key of x by diff and fix the heap.
+	 * Return the number of cuts.
+	 * 
+	 */
+	public int decreaseKey(HeapNode x, int diff) 
+	{    
+		return 46; // should be replaced by student code
+	}
+
+	/**
+	 * 
+	 * Delete the x from the heap.
+	 * Return the number of links.
+	 *
+	 */
+	public int delete(HeapNode x) 
+	{    
+		return 46; // should be replaced by student code
+	}
+
+
+	/**
+	 * 
+	 * Return the total number of links.
+	 * 
+	 */
+	public int totalLinks()
+	{
+		return this.linkCount; // should be replaced by student code
+	}
+
+
+	/**
+	 * 
+	 * Return the total number of cuts.
+	 * 
+	 */
+	public int totalCuts()
+	{
+		return this.cutCount; // should be replaced by student code
+	}
+
+
+	/**
+	 * 
+	 * Meld the heap with heap2
+	 *
+	 */
+	public void meld(FibonacciHeap heap2)
+	{
+		return; // should be replaced by student code   		
+	}
+
+	/**
+	 * 
+	 * Return the number of elements in the heap
+	 *   
+	 */
+	public int size()
+	{
+		return this.size;
+	}
+
+
+	/**
+	 * 
+	 * Return the number of trees in the heap.
+	 * 
+	 */
+	public int numTrees()
+	{
+		return this.rootList.size(); // should be replaced by student code
+	}
+
+	/**
+	 * Class implementing a node in a Fibonacci Heap.
+	 *  
+	 */
+	public static class HeapNode{
+		public int key;
+		public String info;
+		public HeapNode child;
+		public HeapNode next;
+		public HeapNode prev;
+		public HeapNode parent;
+		public int rank;
+	}
+}
