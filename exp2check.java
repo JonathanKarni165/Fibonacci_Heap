a4ce6d9724c8cea52f0ebce9ea59e60dac001898
JonathanKarni165 <yonatankarni165@gmail.com>
2025-06-17 19:40:57 +0300

    added testers

--- exp2check.java
+++ exp2check.java
@@ -5,53 +5,31 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
-public class experiment2 {
+public class exp2check {
     public static void main(String[] args) {
-        int[] cArr = {2,3,4,10,20,100,1000,5000};
-
-        String filePath = "data1.csv";
-        try (FileWriter fw = new FileWriter(filePath, true))
-        {
-            for (int i = 0; i < cArr.length; i++) {
-                double[] CData = {0.0, 0.0, 0.0, 0.0, 0.0};
-
-                for (int j = 0; j < 20; j++) {
-                    double[] curData = run(cArr[i]);
-
-                    for (int k = 0; k < CData.length; k++) {
-                        CData[k] += curData[k];
-                    }
-                    
-                }
-                for (int k = 0; k < CData.length; k++) 
-                    CData[k] /= 20;
-                fw.append(cArr[i] + "," + CData[0] +"," + CData[1] +"," + CData[2] +"," + CData[3] +"," + CData[4] + "\n");
-            }
-        }
-
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        
-        
-    }
-    public static double[] run(int c) {
 
 
         ArrayList<Integer> itemsToInsert = new ArrayList<Integer>();
         ArrayList<FibonacciHeap.HeapNode> nodes = new ArrayList<>(); 
 
-        for (int i = 1; i <= 464646; i++) {
+
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter c value");
+
+        String val = myObj.nextLine();  // Read user input
+
+        int cVal = Integer.parseInt(val);
+        
+
+        for (int i = 1; i <= 50; i++) {
             itemsToInsert.add(i);
         }
         Collections.shuffle(itemsToInsert);
 
         long startTime1 = System.nanoTime();
 
-        FibonacciHeap fb = new FibonacciHeap(c);
-        for (int i = 0; i < 464646; i++) {
+        FibonacciHeap fb = new FibonacciHeap(2);
+        for (int i = 0; i < 50; i++) {
             nodes.add(fb.insert(itemsToInsert.get(i), null));
         }
 
@@ -66,16 +44,27 @@ public class experiment2 {
 
         //now delete max until 46 left
         int i=0;
-        while (nodes.get(i).key > 47) {
+        while (nodes.get(i).key > 5) {
             fb.decreaseKey(nodes.get(i), nodes.get(i).key);
+            
+            // if(fb.size() < 5){
+            //     System.out.println("************************************************************************");
+            //     fb.printHeap();
+            //     System.out.println("************************************************************************");
+            // }
+
+            // System.out.println("************************************************************************");
+            // fb.printHeap();
+            // System.out.println("************************************************************************");
+
             i++;
         }
+
         fb.deleteMin();
-        
+        PrintHeap.printFibonacciHeap(fb);
 
         long endTime2 = System.nanoTime();
         double time = ((endTime1-startTime1)+ (endTime2-startTime2))/1000000;
-
         System.out.println("time: " + (double)(time/1000000));
         System.out.println("size: " + fb.size());
         System.out.println("total links: " + fb.totalLinks());
@@ -83,10 +72,13 @@ public class experiment2 {
         System.out.println("number of trees: " + fb.numTrees());
 
 
-        String filePath = "data.csv";
-        String data = time + "," + fb.size() + "," + fb.totalLinks() + "," + fb.totalCuts() + "," + fb.numTrees()+ "," + c;
-        double[] dataArr = {time,fb.size(),fb.totalLinks(),fb.totalCuts(),fb.numTrees()};
-        return dataArr;
+        String filePath = "data1.csv";
+        String data = time + "," + fb.size() + "," + fb.totalLinks() + "," + fb.totalCuts() + "," + fb.numTrees()+ "," + cVal;
+        try (FileWriter fw = new FileWriter(filePath, true)) {
+            fw.append(data + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
         /*
